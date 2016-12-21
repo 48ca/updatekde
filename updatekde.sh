a04f1395aa892bce8e5ac44112622d0a97a0c9a9
James Houghton <jamesthoughton@gmail.com>
2016-12-21 00:32:52 +0000

    Make the script safer

--- updatekde.sh
+++ updatekde.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ "$UID" -ne "0" ]; then
     echo "This script must be executed as root"
     exit
@@ -8,6 +10,9 @@ fi
 # Set Spotify and Chromium scale factors
 for file in /usr/share/applications/spotify.desktop /usr/share/applications/chromium.desktop
 do
+    if ! [ -f $file ]; then
+        continue
+    fi
     # Removes only one --force-device-scale-factor declaration
     # if it exists and places a new declaration in
     sed -i'.bak' $file \
@@ -17,12 +22,18 @@ do
 done
 
 # Remove KDE logo from splash screen
-sed -i'.bak' \
-    /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/splash/Splash.qml \
-    -e 's/\n\s*source: "images\/kde.svgz"//g'
+if [ -f /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/splash/Splash.qml ]
+then
+    sed -i'.bak' \
+        /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/splash/Splash.qml \
+        -e 's/\n\s*source: "images\/kde.svgz"//g'
+fi
 
 # Rename GNOME's terminal emulator so it doesn't
 # conflict with Xfce4's terminal emulator in krunner
-sed -i'.bak' \
-    /usr/share/applications/org.gnome.Terminal.desktop \
-    -e 's/^\(Name=\).*$/\1GNOME Terminal/g'
+if [ -f /usr/share/applications/org.gnome.Terminal.desktop ]
+then
+    sed -i'.bak' \
+        /usr/share/applications/org.gnome.Terminal.desktop \
+        -e 's/^\(Name=\).*$/\1GNOME Terminal/g'
+fi
